0ef10f4a3d4de678e73084583ae451e6b2e9ab5a
Deb4Dev <chakrabortydebargha575@gmail.com>
2024-04-05 23:33:53 +0530

    inital

---
+++ stl.cpp
@@ -0,0 +1,23 @@
+
+/*template<class x , class y>
+class array{
+
+}*/
+// to create using array object in this template formnat we need to give the values of placeolders which are x nad y
+//#include<array>
+//array<int,4> obj;//y is giving sizeof arr
+//giving us a static array
+#include<iostream>
+#include<array>
+#include<pair>
+using namespace std;
+int main(){
+    array<int,5>data_array = {1,2,3,4,5};
+    cout<<data_array[3];
+    return 0;
+}
+
+pair<t1,t2>pair1
+//here we can give any data type or class in space of t1 or t2
+example
+pair<string,int>p1;
